bb0a433f69149b8e63a74ed1cace7b40b4ff6e83
harnotcy <harnotcy@163.com>
2023-03-20 19:58:28 +0800

    change icon

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import calc
 # main函数入口
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("D:\Code\Qt\VentilationDuctResistanceCalculator\wind.ico"))  # 设置应用 window icon, 为了pyinstaller打包时正常，此处设定为绝对路径
+    app.setWindowIcon(QIcon("D:\Code\Qt\VentilationDuctResistanceCalculator\icon.ico"))  # 设置应用 window icon, 为了pyinstaller打包时正常，此处设定为绝对路径
     Win = calc.MainWindow()
     Win.show()
     sys.exit(app.exec())
